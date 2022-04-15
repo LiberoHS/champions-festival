bf99978917ac48fbd032a54487a8c0ba05eceab9
Jeremy Lim <jeremy.lim1636@gmail.com>
2022-04-15 19:31:18 +1000

    CHF-3: Added Spring Boot Kotlin dependencies

---
+++ src/main/kotlin/ptcg/championsfestival/ChampionsFestivalApplication.kt
@@ -0,0 +1,11 @@
+package ptcg.championsfestival
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChampionsFestivalApplication
+
+fun main(args: Array<String>) {
+	runApplication<ChampionsFestivalApplication>(*args)
+}
---
+++ src/test/kotlin/ptcg/championsfestival/ChampionsFestivalApplicationTests.kt
@@ -0,0 +1,13 @@
+package ptcg.championsfestival
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChampionsFestivalApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
