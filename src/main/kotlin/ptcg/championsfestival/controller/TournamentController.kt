0982c8aae8008158f476229cbe2a5e418f51993b
Jeremy Lim <jeremy.lim1636@gmail.com>
2022-04-18 02:13:37 +1000

    CHF-000: Fixed importing

--- src/main/kotlin/ptcg/championsfestival/controller/TournamentController.kt
+++ src/main/kotlin/ptcg/championsfestival/controller/TournamentController.kt
@@ -3,7 +3,14 @@ package ptcg.championsfestival.controller
 import mu.KotlinLogging
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import ptcg.championsfestival.domain.DomainFactory
 import ptcg.championsfestival.model.NewTournamentRequest
 import ptcg.championsfestival.model.Tournament
